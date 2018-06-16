0e8222c3a859309cf7e6f7f19c036e058f3570a3
Bassem <Bassemfg@gmx.net>
2018-06-15 19:29:53 -0700

    1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint;
 using System.IO;
+using System.Configuration;
 
 namespace ConsoleApp1
 {
@@ -12,9 +13,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int i = 0;
+
+            string RootSiteCollection = System.Configuration.ConfigurationSettings.AppSettings["RootSiteCollection"];
+            //int i = 0;
             int j = 0;
-            Microsoft.SharePoint.SPSite siteColl = new SPSite(@"https://ic3.icat.org.au/group/catprojects/");//http://sp2013vm/
+            Microsoft.SharePoint.SPSite siteColl = //new SPSite(@"https://ic3.icat.org.au/group/catprojects/");
+                new SPSite(RootSiteCollection);
             StringBuilder sbFields = new StringBuilder();
             StringBuilder sbVals = new StringBuilder();
 
@@ -28,43 +32,49 @@ namespace ConsoleApp1
                 foreach (SPList list in web.Lists)
                 {
                     sbVals.Length = 0;
-                    if (list.BaseType == SPBaseType.DocumentLibrary && list.Hidden==false )
+                    if (list.BaseType == SPBaseType.DocumentLibrary && list.Hidden == false)
                     {
                         j = 0;
                         foreach (SPItem item in list.Items)
                         {
 
-                            i = 0;
+
                             foreach (SPField field in item.Fields)
                             {
-                                if (field.Hidden == false && field.Sealed == false)
+                                try
                                 {
-                                    if (j == 0)
+                                    if (field.Hidden == false && field.Sealed == false)
                                     {
-                                        sbFields.Append(field.ToString());
-                                        sbFields.Append(',');
+                                        if (j == 0)
+                                        {
+                                            sbFields.Append(field.ToString());
+                                            sbFields.Append(',');
+                                        }
+                                        if (item[field.ToString()] != null)
+                                            sbVals.Append(item[field.ToString()].ToString());
+                                        sbVals.Append(',');
+
                                     }
-                                    if (item[i] != null)
-                                        sbVals.Append(item[i].ToString());
-                                    sbVals.Append(',');
-                                    i++;
                                 }
+                                catch { }
                             }
                             //remove last ','
-                            sbFields.Length = sbFields.Length - 1;
-                            sbVals.Length = sbVals.Length - 1;
+                            if (sbFields.Length > 0)
+                                sbFields.Length = sbFields.Length - 1;
+                            if (sbVals.Length > 0)
+                                sbVals.Length = sbVals.Length - 1;
                             // add new lines
                             sbFields.Append(@"
-");                         sbVals.Append(@"
+"); sbVals.Append(@"
 ");
                             j++;
+
                         }
                     }
-
-                    sbFields.Append(sbVals.ToString());
-                    sbFields.Append(@"
+                        sbFields.Append(sbVals.ToString());
+                        sbFields.Append(@"
 ");
-
+                    
                 }
 
                 sw = new StreamWriter(@"c:\metadata_" + web.Url.Substring(web.Url.LastIndexOf(@"/")+1) + @".csv");
